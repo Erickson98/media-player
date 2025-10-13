1c26771abe57ec53e22c244b131d08e1cdf6eaa0
eri <erickson.ruck07@gmail.com>
2025-10-12 21:44:27 -0400

    chore: add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import path from "path";
-import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   output: "server",
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   vite: {
     build: {
       minify: "esbuild",
